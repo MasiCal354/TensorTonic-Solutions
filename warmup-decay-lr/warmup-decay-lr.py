cb86696fcdbfd6a878820fe23c0b55b79ff73723
Faisal Malik Widya Prasetya <faisal.prasetya@toptal.com>
2026-08-29 16:21:42 +0700

    Solve warmup-decay-lr

---
+++ warmup-decay-lr/warmup-decay-lr.py
@@ -0,0 +1,5 @@
+def warmup_decay_schedule(base_lr: float, warmup_steps: int, total_steps: int, current_step: int) -> float:
+    """
+    Returns the learning rate for the requested training step.
+    """
+    return (base_lr * current_step / warmup_steps) if current_step < warmup_steps else (base_lr * (total_steps - current_step)/(total_steps - warmup_steps))
